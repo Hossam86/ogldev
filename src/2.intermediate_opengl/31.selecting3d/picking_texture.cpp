d1b2157c0c47b824023fbb4ae40ebbe7fc2aff46
Hossam Samir <hossamsamir@outlook.com>
2024-01-24 17:39:45 +0200

    convert indentation to tabs

--- src/2.intermediate_opengl/31.selecting3d/picking_texture.cpp
+++ src/2.intermediate_opengl/31.selecting3d/picking_texture.cpp
@@ -11,33 +11,33 @@ Picking_Texture::~Picking_Texture()
 
 void Picking_Texture::init(unsigned int width, unsigned int height)
 {
-    // create FBO;
-    glGenFramebuffers(1, &m_fbo);
-    glBindFramebuffer(GL_FRAMEBUFFER, m_fbo);
+	// create FBO;
+	glGenFramebuffers(1, &m_fbo);
+	glBindFramebuffer(GL_FRAMEBUFFER, m_fbo);
 
-    // create the texture object for the primitive information buffer
-    glGenTextures(1, &m_picking_texture);
-    glBindTexture(GL_TEXTURE_2D, m_picking_texture);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32UI, width, height, 0, GL_RGB_INTEGER, GL_UNSIGNED_INT, NULL);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_picking_texture, 0);
+	// create the texture object for the primitive information buffer
+	glGenTextures(1, &m_picking_texture);
+	glBindTexture(GL_TEXTURE_2D, m_picking_texture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32UI, width, height, 0, GL_RGB_INTEGER, GL_UNSIGNED_INT, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_picking_texture, 0);
 
-    // create the texture object for the depth buffer
-    glGenTextures(1, &m_depth_texture);
-    glBindBuffer(GL_TEXTURE_2D, m_depth_texture);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, width, height, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, m_depth_texture, 0);
+	// create the texture object for the depth buffer
+	glGenTextures(1, &m_depth_texture);
+	glBindBuffer(GL_TEXTURE_2D, m_depth_texture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, width, height, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, m_depth_texture, 0);
 
-    // verify that fbo is correct
-    GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+	// verify that fbo is correct
+	GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
 
-    if (status != GL_FRAMEBUFFER_COMPLETE)
-    {
-        printf("FB error, status: 0x%x\n", status);
-        exit(1);
-    }
-    // restore the default frame buffer
-    glBindTexture(GL_TEXTURE_2D, 0);
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	if (status != GL_FRAMEBUFFER_COMPLETE)
+	{
+		printf("FB error, status: 0x%x\n", status);
+		exit(1);
+	}
+	// restore the default frame buffer
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
